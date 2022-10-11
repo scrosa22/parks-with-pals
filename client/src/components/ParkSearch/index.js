c20e28f19a6954d2c5eba4cd2c79234031d98748
Ortal Wikoff <ortalwikoff@Ortals-MacBook-Air.local>
2022-10-11 19:26:08 -0400

    updated serch component

--- client/src/components/ParkSearch/index.js
+++ client/src/components/ParkSearch/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 function ParkSearch() {
   return (
-<div class="input-group mb-3">
-  <input type="text" class="form-control form-control-lg rounded" value="State" aria-label="readonly input example" readonly />
-  <button type="button" class="btn btn-outline-primary">Search for a park</button>
-</div>
+<div class="container">
+<h3>National Park Finder</h3>
+<form class="js-form">
+<label for="search-term">Enter one or more state abbreviations, separated by commas: </label>
+<input type="text" class="form-control form-control-lg rounded" value="State" aria-label="readonly input example" readonly />
+<label for="max-results">Maximum number of results to return: </label>
+<input type="number" name="max-results" id="js-max-results" value="10"/>
+<input type="submit" value="Go!"></input>
 
+</form>
+<br></br>
+<button type="button" class="btn btn-outline-primary">Search for a park</button>
+
+
+</div>
 
   )
 }
