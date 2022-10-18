36de50afdc1b6eed6b62424bab900fe3af66a7ef
steven rosa <scrosa13@outlook.com>
2022-10-18 12:05:40 -0400

    updating search bar

--- client/src/components/ParkSearch/index.js
+++ client/src/components/ParkSearch/index.js
@@ -58,8 +58,12 @@ const SearchParks = () => {
                 onChange={(e) => setSearchInput(e.target.value)}
                 type="text"
                 size="lg"
-                placeholder="Search for a park"
+                placeholder="Search by state abbr."
               />
+            </Col>
+            <Col>
+              <p>AL,AK,AZ,AR,CA,CO,CT,DE,FL,GA,HI,ID,IL,IN,IA,KS,KY,LA,ME,MD,MA,MI,MN,MS,MO,<br>
+              </br>MT,NE,NV,NH,NJ,NM,NY,NC,ND,OH,OK,OR,PA,RI,SC,SD,TN,TX,UT,VT,VA,WA,WV,WI,WY</p>
             </Col>
             <Col>
               <Button type="submit" variant="success" size="lg">
